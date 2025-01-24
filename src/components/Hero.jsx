bc1bad8a4e8b9559bcea0bac23a69b5ed17abe6c
Saransh Mishra <saranshmishramishra@gmail.com>
2025-01-24 21:36:18 +0530

    removed the computer canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,5 @@
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -24,8 +22,6 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
-      <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
