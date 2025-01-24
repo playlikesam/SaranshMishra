4c8d8795771bfded6a8c4959cb930dcd6d6f0516
Saransh Mishra <saranshmishramishra@gmail.com>
2025-01-24 23:18:25 +0530

    updated the tech stack

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,12 +3,17 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <img src={technology.iconUrl} alt={technology.name} className="w-full h-full object-contain" />
-        </div>
-      ))}
+    <div>
+      <h2 className="text-left text-6xl font-bold mb-6">Technologies</h2>
+      <p className="text-left text-lg text-secondary mb-10">
+        Here are some of the technologies I have worked with. </p>
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <img src={technology.iconUrl} alt={technology.name} className="w-full h-full object-contain" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
